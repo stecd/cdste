7caa7d84b6b313ddee06119d00e618c1a46f00a9
Stephanie Claudino Daffara <stephaniecd@berkeley.edu>
2018-07-16 18:45:33 -0700

    Updated portfolio to include more info

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ $('#linkedin').hover(function(event) {
     resetUI()
 });
 
-$('#blog').hover(function(event) {
+$('#about').hover(function(event) {
   $('.fill').css('color', '#0f8f7a').css('box-shadow', 'inset 0 -2px 0 #0f8f7a');
-  $('#blog').css('color', 'whitesmoke');
+  $('#about').css('color', 'whitesmoke');
   }, function(event) {
     resetUI()
 });
@@ -29,6 +29,24 @@ $('#portfolio').hover(function(event) {
 function resetUI() {
     $('#github').css('color', 'rgba(23, 21, 21, 1)').css('box-shadow', 'inset 0 -2px 0 rgba(23, 21, 21, 1)');
     $('#linkedin').css('color', 'rgba(26, 132, 188, 1)').css('box-shadow', 'inset 0 -2px 0 rgba(26, 132, 188, 1)');
-    $('#blog').css('color', '#0f8f7a').css('box-shadow', 'inset 0 -2px 0 #0f8f7a');
+    $('#about').css('color', '#0f8f7a').css('box-shadow', 'inset 0 -2px 0 #0f8f7a');
     $('#portfolio').css('color', 'rgba(208,91,70,1)').css('box-shadow', 'inset 0 -2px 0 rgba(208,91,70,1)');
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("toTop").style.display = "block";
+  } else {
+    document.getElementById("toTop").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  $('body, html').animate({
+        scrollTop: 0
+  }, 800);
 }
