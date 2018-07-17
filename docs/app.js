b9352051d6190e09f97f2e67c07e880a305d882c
Stephanie Claudino Daffara <stephaniecd@berkeley.edu>
2018-07-17 01:46:32 -0700

    fixed minor ui bugs

--- docs/app.js
+++ docs/app.js
@@ -5,9 +5,9 @@ $('#github').hover(function(event) {
     resetUI()
   });
 
-$('#linkedin').hover(function(event) {
+$('#research').hover(function(event) {
   $('.fill').css('color', 'rgba(26, 132, 188, 1)').css('box-shadow', 'inset 0 -2px 0 rgba(26, 132, 188, 1)');
-  $('#linkedin').css('color', 'whitesmoke');
+  $('#research').css('color', 'whitesmoke');
   }, function(event) {
     resetUI()
 });
@@ -26,9 +26,11 @@ $('#portfolio').hover(function(event) {
     resetUI()
 });
 
+
+
 function resetUI() {
     $('#github').css('color', 'rgba(23, 21, 21, 1)').css('box-shadow', 'inset 0 -2px 0 rgba(23, 21, 21, 1)');
-    $('#linkedin').css('color', 'rgba(26, 132, 188, 1)').css('box-shadow', 'inset 0 -2px 0 rgba(26, 132, 188, 1)');
+    $('#research').css('color', 'rgba(26, 132, 188, 1)').css('box-shadow', 'inset 0 -2px 0 rgba(26, 132, 188, 1)');
     $('#about').css('color', '#0f8f7a').css('box-shadow', 'inset 0 -2px 0 #0f8f7a');
     $('#portfolio').css('color', 'rgba(208,91,70,1)').css('box-shadow', 'inset 0 -2px 0 rgba(208,91,70,1)');
 }
